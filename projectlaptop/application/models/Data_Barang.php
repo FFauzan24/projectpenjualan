adaec8cf9bac6e2f0f502472735e61ecef550ae9
FFauzan24 <102279506+FFauzan24@users.noreply.github.com>
2022-06-25 23:33:30 +0700

    Add files via upload

--- projectlaptop/application/models/Data_Barang.php
+++ projectlaptop/application/models/Data_Barang.php
@@ -4,16 +4,34 @@ class Data_Barang extends CI_model
     public function getalldata()
     {
         return $this->db->get('data_barang')->result_array();
+        $this->db->where('id_barang > 0');
     }
     public function tambahDataBarang()
     {
+        //upload photo
+        $config['max_size'] = 2048;
+        $config['allowed_types'] = "png|jpg|jpeg|gif";
+        $config['remove_spaces'] = TRUE;
+        $config['overwrite'] = TRUE;
+        $config['upload_path'] = FCPATH . 'images';
+        $file = $this->input->post('gambar');
+
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+
+        $this->upload->do_upload('gambar');
+        $data_image = $this->upload->data('gambar');
+        $location = base_url() . 'assets/data_barang/';
+        $pict = $location . $data_image;
+
         $data = array(
             "id_barang" => null,
             "Nama" => $this->input->post('nama', true),
             "jenis" => $this->input->post('jenis', true),
             "detail" => $this->input->post('detail', true),
             "harga" => $this->input->post('harga', true),
-            "stok" => $this->input->post('stok', true)
+            "stok" => $this->input->post('stok', true),
+            "gambar" =>  $pict
         );
 
         $this->db->insert('data_barang', $data);
@@ -32,6 +50,7 @@ class Data_Barang extends CI_model
             "detail" => $this->input->post('detail', true),
             "harga" => $this->input->post('harga', true),
             "stok" => $this->input->post('stok', true)
+
         );
         $this->db->where('id_barang', $this->input->post('id'));
         $this->db->update('data_barang', $data);
@@ -45,5 +64,18 @@ class Data_Barang extends CI_model
         $cari = $this->input->post('cari');
         $this->db->like('Nama', $cari);
         return $this->db->get('data_barang')->result_array();
+    }
+    public function tambahDatakeranjang()
+    {
+        $data = array(
+            "id_barang" => null,
+            "Nama" => $this->input->post('nama', true),
+            "jenis" => $this->input->post('jenis', true),
+            "detail" => $this->input->post('detail', true),
+            "harga" => $this->input->post('harga', true),
+            "stok" => $this->input->post('stok', true)
+        );
+
+        $this->db->insert('data_barang', $data);
     }
 }
